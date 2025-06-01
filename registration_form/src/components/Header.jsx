d951e09e9843e7b2d47a6103b65989da6938a9b2
Bina <dyakovabina@gmail.com>
2025-06-01 23:19:49 +0200

    Change design

---
+++ registration_form/src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Good to see you again!</h1>
+      <p>Log in to continue.</p>
+    </header>
+  );
+}
